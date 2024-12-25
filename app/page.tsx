7c243cac16d2946d4f8dc06d404c9b7f187f3b2f
kai890707 <z85385637@gmail.com>
2024-12-25 11:41:02 +0800

    branch test-1225

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         />
         <ol>
           <li>
-            Get started by editing <code>app/page.tsx</code>.
+            Get started by editing <code>I changed this file in here</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
